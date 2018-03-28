b3e1caaae1d44beb7025a89dbdab09064aef2122
Lei-surely <2369934355@qq.com>
2018-03-28 22:41:35 +0800

    创建仓库

---
+++ src/实验三/MainTest.java
@@ -0,0 +1,20 @@
+
+import javax.swing.*;
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+public class MainTest {
+	public static  void main(String[] args) {
+		MyFrame frame = new MyFrame("Àƒ‘Ú‘ÀÀ„");
+	}
+
+}
+
---
+++ src/实验三/MyFrame.java
@@ -0,0 +1,167 @@
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.StringTokenizer;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.Timer;
+
+class MyFrame extends JFrame {
+		private JTextArea area=null;
+		public JTextField field[];
+		private JButton btn_tj =null;
+		private JButton btn_begin =null;
+		private JButton btn_js =null;
+		private JPanel p_area = null;
+		private JPanel p_btn = null;
+		private GridLayout layout =null;
+
+		Date now = new Date();
+					
+		JLabel lbl=null;//显示时间
+		public MyFrame(String title) {
+			super(title);
+			init();
+			registerListener();
+		}
+		public void init() {
+			now.setHours(0);
+			now.setMinutes(0);
+			now.setSeconds(0);
+			 
+			area=new JTextArea();
+			area.setFont(new Font("宋体",Font.BOLD,20));
+			
+			layout = new GridLayout();
+
+			btn_tj = new JButton("提交");
+			btn_js = new JButton("统计结果");
+			btn_begin = new JButton("开始测试");
+			
+			p_area= new JPanel();
+			p_btn=new JPanel();
+			p_btn.setBackground(Color.LIGHT_GRAY);
+			
+			lbl = new JLabel("计时区域");
+		    lbl.setBackground(Color.black);
+			p_area.setLayout(layout);
+			p_area.add(area);
+			
+			this.add(p_area,BorderLayout.CENTER);
+			
+			p_btn.add(btn_begin);
+			p_btn.add(btn_tj);
+			p_btn.add(btn_js);
+			p_btn.add(lbl);
+			this.add(p_btn,BorderLayout.SOUTH);
+			
+			this.setBackground(Color.BLUE);
+			this.setBounds(100,20,1000,700);
+			this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+			this.setVisible(true);
+		}
+		
+		//注册监听
+		private void registerListener() {
+			Timer timer = new Timer(1000, new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					Date now2 = new Date(now.getTime() + 1000);
+					now = now2;
+					SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+					lbl.setText(formatter.format(now));
+				}
+			});
+			btn_begin.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					//开始测试
+					begin();
+					timer.start();
+				}				
+			});
+			btn_tj.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+
+					timer.stop();
+					try {
+						submit();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			});
+		}
+		//开始测试函数
+		public void begin() {
+			
+			//运算代码
+			CalTest calrandom= new CalTest();
+			try {
+				calrandom.random();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			File file = new File("suanshi.txt");
+		    if (file.exists() && file.isFile()) {
+		    	try {
+		            BufferedReader input = new BufferedReader(new FileReader(file));
+		            String text;
+		            while ((text = input.readLine()) != null)
+		            	area.setText(area.getText() + text + "\n");
+		    	} catch (IOException ioException) {
+		    	System.err.println("File Error!");
+		       }		
+		    }	    
+		    //计时
+		}
+		
+		//提交函数
+		public void submit() throws IOException {
+			FileWriter fw = null;
+			try {
+				fw = new FileWriter("result.txt");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String str=area.getText();
+			for(int i=0;i<str.length();i++){
+			 if(str.charAt(i)==10){
+			     fw.write(13);//写入\r
+			     fw.write(10);//写入\n
+			 }else{
+			    fw.write(str.charAt(i));
+			        }
+			}
+			   fw.close();			
+			
+			this.dispose();
+			SubFrame subframe= new SubFrame("结果");
+		}
+		
+		
+	
+		
+	}
+		
